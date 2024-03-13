2031aa5afe5dfe50fa944b37c00a0b8f27e06c82
Pann Sengpanich <panncraftyo@gmail.com>
2024-03-13 13:50:29 +0700

    implement basic google login button

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,11 +4,29 @@ import { useQuery } from "@tanstack/react-query";
 import { getProducts } from "../services/queries";
 import NavigationBar from "../Components/NavigationBar";
 import styles from "../sass/HomePage.module.scss";
+import { GoogleLogin } from "@react-oauth/google";
+import { useGoogleLogin } from "@react-oauth/google";
+import { jwtDecode } from "jwt-decode";
 
 function HomePage() {
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => console.log(tokenResponse),
+  });
   return (
     <div className={styles.container}>
-      <div>HOMEPAGE</div>
+      <div className={styles.header}>HOMEPAGE</div>
+      <GoogleLogin
+        onSuccess={(credentialResponse) => {
+          const credentialResponseDecoded = jwtDecode(
+            credentialResponse.credential
+          );
+          console.log(credentialResponseDecoded);
+        }}
+        onError={() => {
+          console.log("Login Failed");
+        }}
+      />
+      <button onClick={() => login()}>Sign in with Google 🚀</button>
     </div>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,16 +5,19 @@ import App from "./App.jsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { MantineProvider } from "@mantine/core";
+import { GoogleOAuthProvider } from "@react-oauth/google";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <MantineProvider>
-        <App></App>
-        <ReactQueryDevtools />
-      </MantineProvider>
+      <GoogleOAuthProvider clientId="917129421762-96acbas5dotojmpvijdq0ost9odb2nmo.apps.googleusercontent.com">
+        <MantineProvider>
+          <App></App>
+          <ReactQueryDevtools />
+        </MantineProvider>
+      </GoogleOAuthProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
